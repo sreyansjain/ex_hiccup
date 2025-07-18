3da26da1c5a8239b0eb7e47ba23fa3f617edb34e
Sreyans Jain <sreyansjain@gmail.com>
2025-07-18 22:39:59 +0530

    Bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExHiccup.MixProject do
   def project do
     [
       app: :ex_hiccup,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
